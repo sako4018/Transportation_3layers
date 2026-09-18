f098879a10194a4183c979b60a95adc4bb90ea2a
Sarkis Demirdjian <sarkdemi@gmail.com>
2026-09-18 20:28:00 +0300

    new class: added database prices

---
+++ Transportation-3layers/DAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportation_3layers
+{
+    internal class DAL
+    {
+        public double GetTaxiDayPrice()
+        {
+            return 0.79;
+        }
+        public double GetTaxiNightPrice()
+        {
+            return 0.90;
+        }
+        public double GetBusPrice()
+        {
+            return 0.09;
+        }
+        public double GetTrainPrice()
+        {
+            return 0.06;
+        }
+    }
+}
