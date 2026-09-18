1ce0897e1b5a6c3879ea7f2f443135dacfa59648
Sarkis Demirdjian <sarkdemi@gmail.com>
2026-09-18 20:47:34 +0300

    v1: ready

--- Transportation-3layers/PresentationLayer.cs
+++ Transportation-3layers/PresentationLayer.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Transportation_3layers
 {
-    internal class Program
+    internal class PresentationLayer
     {
         static void Main(string[] args)
-        {
+        {   
+            //set info
+            Console.Write("Enter the km you want to travel: ");
+            decimal km = Convert.ToDecimal(Console.ReadLine());
+            string dn = "";
+            if (km < 20)
+            {
+                Console.Write("day or night: ");
+                dn = Console.ReadLine();
+            }
+            //get info
+            BuisnessLayer buisnessLayer = new BuisnessLayer();
+            double price = buisnessLayer.CalculatePrice(km, dn);
+            Console.WriteLine(price);
         }
     }
 }
